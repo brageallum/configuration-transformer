5e4843ceb7f6a1d21f33234708e895aa2a19a448
Brage Allum <brageallum@gmail.com>
2021-02-17 19:46:56 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  "rules": {
+    "@typescript-eslint/restrict-template-expressions": "off"
+  }
+};
